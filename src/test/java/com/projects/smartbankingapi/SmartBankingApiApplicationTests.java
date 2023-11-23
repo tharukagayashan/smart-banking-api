0e3f982d5e4218de94760e55d768e1d422bc6540
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-11-24 02:23:55 +0530

    initial commit

---
+++ src/test/java/com/projects/smartbankingapi/SmartBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.smartbankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
