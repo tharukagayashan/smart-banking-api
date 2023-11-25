7318024dee4431139c3d20d90aa648bc48b787d1
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-11-25 13:37:33 +0530

    Master table models

---
+++ src/main/java/com/projects/smartbankingapi/model/master/BnMAccount.java
@@ -0,0 +1,68 @@
+package com.projects.smartbankingapi.model.master;
+
+import com.projects.smartbankingapi.audit.AuditModel;
+import com.projects.smartbankingapi.model.reference.BnRAccountType;
+import com.projects.smartbankingapi.model.reference.BnRBranch;
+import com.projects.smartbankingapi.model.reference.BnRCurrency;
+import com.projects.smartbankingapi.model.reference.BnRStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "BN_M_ACCOUNT", indexes = {
+        @Index(name = "BN_M_ACCOUNT_ACCOUNT_ID_IDX", columnList = "ACCOUNT_ID"),
+        @Index(name = "BN_M_ACCOUNT_ACCOUNT_NO_UNIQUE_IDX", columnList = "ACCOUNT_NO", unique = true),
+        @Index(name = "BN_M_ACCOUNT_CUSTOMER_ID_IDX", columnList = "CUSTOMER_ID"),
+        @Index(name = "BN_M_ACCOUNT_ACCOUNT_TYPE_ID_IDX", columnList = "ACCOUNT_TYPE_ID"),
+        @Index(name = "BN_M_ACCOUNT_STATUS_ID_IDX", columnList = "STATUS_ID"),
+        @Index(name = "BN_M_ACCOUNT_CURRENCY_ID_IDX", columnList = "CURRENCY_ID"),
+        @Index(name = "BN_M_ACCOUNT_BRANCH_ID_IDX", columnList = "BRANCH_ID")
+})
+public class BnMAccount extends AuditModel {
+
+    @Id
+    @SequenceGenerator(name = "BN_M_ACCOUNT", sequenceName = "BN_M_ACCOUNT_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BN_M_ACCOUNT")
+    @Column(name = "ACCOUNT_ID", nullable = false)
+    private Long accountId;
+
+    @Column(name = "ACCOUNT_NO", length = 25, unique = true)
+    private String accountNo;
+
+    @Column(name = "CURRENT_BAL", nullable = false)
+    private Float currentBalance;
+
+    @Column(name = "AVAILABLE_BAL", nullable = false)
+    private Float availableBalance;
+
+    @Column(name = "OPEN_DATE", updatable = false)
+    private LocalDate openedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CUSTOMER_ID", referencedColumnName = "CUSTOMER_ID", nullable = false)
+    private BnMCustomer bnMCustomer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_TYPE_ID", referencedColumnName = "ACCOUNT_TYPE_ID", nullable = false)
+    private BnRAccountType bnRAccountType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STATUS_ID", referencedColumnName = "STATUS_ID", nullable = false)
+    private BnRStatus bnRStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CURRENCY_ID", referencedColumnName = "CURRENCY_ID", nullable = false)
+    private BnRCurrency bnRCurrency;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BRANCH_ID", referencedColumnName = "BRANCH_ID", nullable = false)
+    private BnRBranch bnRBranch;
+
+}
---
+++ src/main/java/com/projects/smartbankingapi/model/master/BnMLoan.java
@@ -0,0 +1,76 @@
+package com.projects.smartbankingapi.model.master;
+
+import com.projects.smartbankingapi.audit.AuditModel;
+import com.projects.smartbankingapi.model.reference.BnRLoanProduct;
+import com.projects.smartbankingapi.model.reference.BnRStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "BN_M_LOAN", indexes = {
+        @Index(name = "BN_M_LOAN_LOAN_ID_IDX", columnList = "LOAN_ID"),
+        @Index(name = "BN_M_LOAN_STATUS_ID_IDX", columnList = "STATUS_ID"),
+        @Index(name = "BN_M_LOAN_CUSTOMER_ID_IDX", columnList = "CUSTOMER_ID"),
+        @Index(name = "BN_M_LOAN_LOAN_PRODUCT_ID_IDX", columnList = "LOAN_PRODUCT_ID")
+})
+public class BnMLoan extends AuditModel {
+
+    @Id
+    @SequenceGenerator(name = "BN_M_LOAN", sequenceName = "BN_M_LOAN_SEQ", allocationSize = 1)
+    @GeneratedValue(generator = "BN_M_LOAN", strategy = GenerationType.SEQUENCE)
+    @Column(name = "LOAN_ID", nullable = false)
+    private Long loanId;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private Float amount;
+
+    @Column(name = "INTEREST", nullable = false)
+    private Float interest;
+
+    @Column(name = "TOT_INSTALLMENTS")
+    private Integer totInstallments;
+
+    @Column(name = "NEXT_INSTALLMENT_DATE")
+    private LocalDate nextInstallmentDate;
+
+    @Column(name = "TOT_ARREARS_AMT", nullable = false)
+    private Float totArrearsAmt;
+
+    @Column(name = "REM_INSTALLMENTS")
+    private Integer remInstallments;
+
+    @Column(name = "NEXT_INSTALLMENT_AMT", nullable = false)
+    private Float nextInstallmentAmt;
+
+    @Column(name = "DISTRIBUTED_AMT", nullable = false)
+    private Float distributedAmt;
+
+    @Column(name = "TOT_SETTLED_AMT", nullable = false)
+    private Float totSettledAmt;
+
+    @Column(name = "TOT_INTEREST_PAID", nullable = false)
+    private Float totInterestPaid;
+
+    @Column(name = "TOT_PAID", nullable = false)
+    private Float totPaid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STATUS_ID", referencedColumnName = "STATUS_ID", nullable = false)
+    private BnRStatus bnRStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CUSTOMER_ID", referencedColumnName = "CUSTOMER_ID", nullable = false)
+    private BnMCustomer bnMCustomer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "LOAN_PRODUCT_ID", referencedColumnName = "LOAN_PRODUCT_ID", nullable = false)
+    private BnRLoanProduct bnRLoanProduct;
+
+}
