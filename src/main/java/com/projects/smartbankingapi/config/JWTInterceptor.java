7f40323f908c30272922c1892103ef1cdacd7c51
Tharuka Gayashan <tharuka.g@informaticsint.com>
2023-11-30 13:32:42 +0530

    JWT Intercepter and WebMvcConfig

--- src/main/java/com/projects/smartbankingapi/config/JWTInterceptor.java
+++ src/main/java/com/projects/smartbankingapi/config/JWTInterceptor.java
@@ -1,15 +1,15 @@
 package com.projects.smartbankingapi.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Service
+@Component
 @Slf4j
-public class JWTIntercepter implements HandlerInterceptor {
+public class JWTInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
--- src/main/java/com/projects/smartbankingapi/config/WebMvcConfig.java
+++ src/main/java/com/projects/smartbankingapi/config/WebMvcConfig.java
@@ -1,19 +1,21 @@
 package com.projects.smartbankingapi.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final JWTIntercepter jwtIntercepter;
+    private final JETInterceptor JETInterceptor;
 
-    public WebMvcConfig(JWTIntercepter jwtIntercepter) {
-        this.jwtIntercepter = jwtIntercepter;
+    public WebMvcConfig(JETInterceptor JETInterceptor) {
+        this.JETInterceptor = JETInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtIntercepter)
+        registry.addInterceptor(JETInterceptor)
                 .excludePathPatterns(
                         "/auth/**",
                         "/swagger-ui/**",
